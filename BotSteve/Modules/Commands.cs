ede6389e13d611e5bb24692b2cada95e1aec31c5
Philippe Py <philippe.patey@hotmail.fr>
2021-08-28 22:15:17 +0200

    Ajoutez des fichiers projet.

---
+++ BotSteve/Modules/Commands.cs
@@ -0,0 +1,26 @@
+﻿using Discord;
+using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotSteve.Modules
+{
+    public class Commands : ModuleBase<SocketCommandContext>
+    {
+        [Command("ping")]
+        public async Task PingAsync() => await ReplyAsync("pong");
+
+        [Command("avatar")]
+        public async Task AvatarAsync(ushort size = 512) => await ReplyAsync(CDN.GetUserAvatarUrl(Context.User.Id, Context.User.AvatarId, size, ImageFormat.Auto));
+
+        [Command("react")]
+        public async Task ReactAsync(string pMessage, string pEmoji)
+        {
+            var message = await Context.Channel.SendMessageAsync(pMessage);
+            await message.AddReactionAsync(new Emoji(pEmoji));
+        }
+    }
+}
---
+++ BotSteve/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using static System.Console;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+using System.Reflection;
+
+namespace BotSteve
+{
+    class Program
+    {
+        private DiscordSocketClient client;
+        private CommandService commands;
+
+
+        static void Main(string[] args) => new Program().RunBotAsync().GetAwaiter().GetResult();
+        
+        public async Task RunBotAsync()
+        {
+            client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                LogLevel = LogSeverity.Debug
+            });
+            client.Log += Log;
+
+            commands = new CommandService();
+            client.Ready += () =>
+            {
+                WriteLine("########## STARTED ##########");
+                return Task.CompletedTask;
+            };
+            await InstallCommandsAsync(); //Attach command
+            await client.LoginAsync(TokenType.Bot, Environment.GetEnvironmentVariable("DiscordToken", EnvironmentVariableTarget.User));
+            await client.StartAsync();
+            await Task.Delay(-1);
+        }
+
+        private Task Log(LogMessage arg)
+        {
+           WriteLine(arg.ToString());
+            return Task.CompletedTask;
+        }
+
+        public async Task InstallCommandsAsync()
+        {
+            client.MessageReceived += HandleCommandAsync;
+            await commands.AddModulesAsync(Assembly.GetEntryAssembly(), null);
+        }
+
+        private async Task HandleCommandAsync(SocketMessage pMessage)
+        {
+            int argPos = 0;
+            var message = (SocketUserMessage)pMessage;
+            if (message == null) //Command cannot be null
+                return;
+            if (!message.HasCharPrefix('!', ref argPos)) //Command has to have '!' before the text
+                return;
+            var context = new SocketCommandContext(client, message);
+            var result = await commands.ExecuteAsync(context, argPos, null);
+            if (!result.IsSuccess) //Command is not valid
+                await context.Channel.SendMessageAsync(result.ErrorReason);
+        }
+    }
+}
